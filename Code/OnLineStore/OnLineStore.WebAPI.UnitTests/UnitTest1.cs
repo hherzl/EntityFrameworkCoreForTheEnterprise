e7dd144db2b543825ec89eb1dad09cfeef63e0e3
HH <hans.herzl@live.com>
2018-11-03 01:30:02 -0600

    Addition of unit tests project

---
+++ Code/OnLineStore/OnLineStore.WebAPI.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace OnLineStore.WebAPI.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
