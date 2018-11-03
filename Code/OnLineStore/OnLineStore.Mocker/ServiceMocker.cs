9d0f0a68864a3d097f0d144888e01cdb56a071af
HH <hans.herzl@live.com>
2018-11-03 02:12:21 -0600

    Addition of code for mocker

---
+++ Code/OnLineStore/OnLineStore.Mocker/ServiceMocker.cs
@@ -0,0 +1,16 @@
+﻿using OnLineStore.Common;
+using OnLineStore.Core;
+using OnLineStore.Core.BusinessLayer;
+using OnLineStore.Core.BusinessLayer.Contracts;
+
+namespace OnLineStore.Mocker
+{
+    public static class ServiceMocker
+    {
+        public static IProductionService GetProductionService()
+            => new ProductionService(LogHelper.GetLogger<ProductionService>(), new UserInfo("mocker"), DbContextMocker.GetStoreDbContext());
+
+        public static ISalesService GetSalesService()
+            => new SalesService(LogHelper.GetLogger<SalesService>(), new UserInfo("mocker"), DbContextMocker.GetStoreDbContext());
+    }
+}
