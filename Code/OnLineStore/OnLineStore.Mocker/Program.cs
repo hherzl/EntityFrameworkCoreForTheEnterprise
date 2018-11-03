ebe629050c48a70dd47a36313acd6934131f94ea
HH <hans.herzl@live.com>
2018-11-03 01:28:41 -0600

    Addition of Mocker project

---
+++ Code/OnLineStore/OnLineStore.Mocker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OnLineStore.Mocker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
