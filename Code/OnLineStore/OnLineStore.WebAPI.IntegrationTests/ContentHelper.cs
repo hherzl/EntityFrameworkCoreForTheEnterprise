0283405e63d539f28502059bea4475b5853977da
HH <hans.herzl@live.com>
2018-11-03 02:03:59 -0600

    Addition of code for integration tests

---
+++ Code/OnLineStore/OnLineStore.WebAPI.IntegrationTests/ContentHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace OnLineStore.WebAPI.IntegrationTests
+{
+    public static class ContentHelper
+    {
+        public static StringContent GetStringContent(object obj)
+            => new StringContent(JsonConvert.SerializeObject(obj), Encoding.Default, "application/json");
+    }
+}
