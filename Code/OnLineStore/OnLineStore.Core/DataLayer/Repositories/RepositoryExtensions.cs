bc0837699009bc97a231b0df1d61f04f0f7d0a8c
HH <hans.herzl@live.com>
2018-11-11 02:05:30 -0600

    Code review: remove of unused extension methods for DbContext

--- Code/OnLineStore/OnLineStore.Core/DataLayer/Repositories/RepositoryExtensions.cs
+++ Code/OnLineStore/OnLineStore.Core/DataLayer/Repositories/RepositoryExtensions.cs
@@ -1,20 +1,10 @@
-﻿using System;
-using System.Linq;
-using Microsoft.EntityFrameworkCore;
-using OnLineStore.Core.EntityLayer;
+﻿using System.Linq;
 
 namespace OnLineStore.Core.DataLayer.Repositories
 {
     public static class RepositoryExtensions
     {
-        public static IQueryable<TEntity> Paging<TEntity>(this DbContext dbContext, Int32 pageSize = 0, Int32 pageNumber = 0) where TEntity : class, IEntity
-        {
-            var query = dbContext.Set<TEntity>().AsQueryable();
-
-            return pageSize > 0 && pageNumber > 0 ? query.Skip((pageNumber - 1) * pageSize).Take(pageSize) : query;
-        }
-
-        public static IQueryable<TModel> Paging<TModel>(this IQueryable<TModel> query, Int32 pageSize = 0, Int32 pageNumber = 0) where TModel : class
+        public static IQueryable<TModel> Paging<TModel>(this IQueryable<TModel> query, int pageSize = 0, int pageNumber = 0) where TModel : class
             => pageSize > 0 && pageNumber > 0 ? query.Skip((pageNumber - 1) * pageSize).Take(pageSize) : query;
     }
 }
