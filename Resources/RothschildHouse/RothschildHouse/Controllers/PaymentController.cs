2a69beb06bcf9f972f0e8bcef6547661f5987d55
HH <hans.herzl@live.com>
2018-12-30 02:32:57 -0600

    Addition of controller and request for place payment

---
+++ Resources/RothschildHouse/RothschildHouse/Controllers/PaymentController.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RothschildHouse.Requests;
+
+namespace RothschildHouse.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        public PaymentController()
+        {
+        }
+
+        public async Task<IActionResult> PostPayment(PostPaymentRequest request)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Resources/RothschildHouse/RothschildHouse/Requests/PostPaymentRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RothschildHouse.Requests
+{
+    public class PostPaymentRequest
+    {
+        public PostPaymentRequest()
+        {
+        }
+
+        public string CardHolderName { get; set; }
+
+        public string CardType { get; set; }
+
+        public string CardNumber { get; set; }
+
+        public DateTime? ExpirationDate { get; set; }
+
+        public string Cvv { get; set; }
+
+        public decimal? Amount { get; set; }
+    }
+}
