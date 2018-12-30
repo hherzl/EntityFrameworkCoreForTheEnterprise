81f817e179e458375c7630953a6a4afd11c50625
HH <hans.herzl@live.com>
2018-12-30 14:21:40 -0600

    Set port numbers

--- Resources/RothschildHouse/RothschildHouse.IdentityServer/Program.cs
+++ Resources/RothschildHouse/RothschildHouse.IdentityServer/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace RothschildHouse.IdentityServer
 {
@@ -18,7 +11,9 @@ namespace RothschildHouse.IdentityServer
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+            WebHost
+                .CreateDefaultBuilder(args)
+                .UseUrls("http://localhost:1800")
                 .UseStartup<Startup>();
     }
 }
--- Resources/RothschildHouse/RothschildHouse/Program.cs
+++ Resources/RothschildHouse/RothschildHouse/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace RothschildHouse
 {
@@ -18,7 +11,9 @@ namespace RothschildHouse
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+            WebHost
+                .CreateDefaultBuilder(args)
+                .UseUrls("http://localhost:1900")
                 .UseStartup<Startup>();
     }
 }
