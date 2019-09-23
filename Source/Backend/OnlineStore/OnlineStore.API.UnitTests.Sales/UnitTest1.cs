8895c879e8c382716467a7ec12bb3fe05192a5e2
HH <hans.herzl@live.com>
2019-09-22 21:00:01 -0600

    Code refactor: add blank projects

---
+++ Source/Backend/OnlineStore/OnlineStore.API.UnitTests.Sales/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace OnlineStore.API.UnitTests.Sales
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Source/Backend/OnlineStore/OnlineStore.API.UnitTests.Warehouse/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace OnlineStore.API.UnitTests.Warehouse
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
