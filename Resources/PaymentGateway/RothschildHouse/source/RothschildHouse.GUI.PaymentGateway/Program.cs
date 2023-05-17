c98c71fe24c68a3e8bf61f491c25ea99827f3c5f
HH <hans.herzl@live.com>
2023-05-16 19:29:47 -0600

    RothschildHouse-GUI: Addition of MudBlazor project

---
+++ Resources/PaymentGateway/RothschildHouse/source/RothschildHouse.GUI.PaymentGateway/Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using RothschildHouse.GUI.PaymentGateway;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
+
+await builder.Build().RunAsync();
