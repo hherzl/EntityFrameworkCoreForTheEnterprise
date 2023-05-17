744deccb2e396a905b0fb51a3216627d69543415
HH <hans.herzl@live.com>
2023-05-17 03:13:31 -0600

    Addition of API.PaymentGateway project

---
+++ Resources/PaymentGateway/RothschildHouse/source/RothschildHouse.API.PaymentGateway/Controllers/PaymentGatewayController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RothschildHouse.API.PaymentGateway.Controllers
+{
+    [ApiController]
+    [Route("api/v1")]
+    public class PaymentGatewayController
+    {
+        private readonly ILogger<PaymentGatewayController> _logger;
+
+        public PaymentGatewayController(ILogger<PaymentGatewayController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
