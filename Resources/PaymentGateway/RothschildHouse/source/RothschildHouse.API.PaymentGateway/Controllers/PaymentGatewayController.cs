16b9413224fe5a223f56e5d844f77fdb8ebb7425
HH <hans.herzl@live.com>
2023-05-19 05:20:18 -0600

    RouthschildHouse-API: Addition of xml comments for endpoints

--- Resources/PaymentGateway/RothschildHouse/source/RothschildHouse.API.PaymentGateway/Controllers/PaymentGatewayController.cs
+++ Resources/PaymentGateway/RothschildHouse/source/RothschildHouse.API.PaymentGateway/Controllers/PaymentGatewayController.cs
@@ -24,6 +24,14 @@ namespace RothschildHouse.API.PaymentGateway.Controllers
             _mediator = mediator;
         }
 
+        /// <summary>
+        /// Returns the countries that match with the specified search criteria.
+        /// </summary>
+        /// <param name="request">Search parameters</param>
+        /// <returns>The countries.</returns>
+        /// <response code="200">Returns the countries</response>
+        /// <response code="400">If the request is invalid</response>
+        /// <response code="500">If there was an internal error</response>
         [HttpPost("search-country")]
         [ProducesResponseType(200, Type = typeof(IListResponse<CountryItemModel>))]
         [ProducesResponseType(400)]
@@ -35,6 +43,14 @@ namespace RothschildHouse.API.PaymentGateway.Controllers
             return response.ToOkResult();
         }
 
+        /// <summary>
+        /// Returns an existing country by Id.
+        /// </summary>
+        /// <param name="id">Country Id</param>
+        /// <returns>The country.</returns>
+        /// <response code="200">Returns the country</response>
+        /// <response code="404">If the resource doesn't exist</response>
+        /// <response code="500">If there was an internal error</response>
         [HttpGet("country/{id}")]
         [ProducesResponseType(200, Type = typeof(ISingleResponse<CountryDetailsModel>))]
         [ProducesResponseType(404)]
@@ -49,6 +65,14 @@ namespace RothschildHouse.API.PaymentGateway.Controllers
             return response.ToOkResult();
         }
 
+        /// <summary>
+        /// Returns the currencies that match with the specified search criteria.
+        /// </summary>
+        /// <param name="request">Search parameters</param>
+        /// <returns>The currencies.</returns>
+        /// <response code="200">Returns the currencies</response>
+        /// <response code="400">If the request is invalid</response>
+        /// <response code="500">If there was an internal error</response>
         [HttpPost("search-currency")]
         [ProducesResponseType(200, Type = typeof(IListResponse<CurrencyItemModel>))]
         [ProducesResponseType(400)]
@@ -60,6 +84,14 @@ namespace RothschildHouse.API.PaymentGateway.Controllers
             return response.ToOkResult();
         }
 
+        /// <summary>
+        /// Returns an existing currency by Id.
+        /// </summary>
+        /// <param name="id">Currency Id</param>
+        /// <returns>The currency.</returns>
+        /// <response code="200">Returns the currency</response>
+        /// <response code="404">If the resource doesn't exist</response>
+        /// <response code="500">If there was an internal error</response>
         [HttpGet("currency/{id}")]
         [ProducesResponseType(200, Type = typeof(ISingleResponse<CurrencyDetailsModel>))]
         [ProducesResponseType(404)]
@@ -74,6 +106,14 @@ namespace RothschildHouse.API.PaymentGateway.Controllers
             return response.ToOkResult();
         }
 
+        /// <summary>
+        /// Returns the client applications that match with the specified search criteria.
+        /// </summary>
+        /// <param name="request">Search parameters</param>
+        /// <returns>The client applications.</returns>
+        /// <response code="200">Returns the client applications</response>
+        /// <response code="400">If the request is invalid</response>
+        /// <response code="500">If there was an internal error</response>
         [HttpPost("search-client-application")]
         [ProducesResponseType(200, Type = typeof(IListResponse<ClientApplicationItemModel>))]
         [ProducesResponseType(400)]
@@ -85,6 +125,14 @@ namespace RothschildHouse.API.PaymentGateway.Controllers
             return response.ToOkResult();
         }
 
+        /// <summary>
+        /// Returns an existing client application by Id.
+        /// </summary>
+        /// <param name="id">Client application Id</param>
+        /// <returns>The client application.</returns>
+        /// <response code="200">Returns the client application</response>
+        /// <response code="404">If the resource doesn't exist</response>
+        /// <response code="500">If there was an internal error</response>
         [HttpGet("client-application/{id}")]
         [ProducesResponseType(200, Type = typeof(ISingleResponse<ClientApplicationDetailsModel>))]
         [ProducesResponseType(404)]
@@ -99,31 +147,14 @@ namespace RothschildHouse.API.PaymentGateway.Controllers
             return response.ToOkResult();
         }
 
-        [HttpPost("search-customer")]
-        [ProducesResponseType(200, Type = typeof(PagedResponse<CustomerItemModel>))]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(500)]
-        public async Task<IActionResult> SearchCustomersAsync([FromBody] SearchCustomersQuery request)
-        {
-            var response = await _mediator.Send(request);
-
-            return response.ToOkResult();
-        }
-
-        [HttpGet("customer/{id}")]
-        [ProducesResponseType(200, Type = typeof(ISingleResponse<CustomerDetailsModel>))]
-        [ProducesResponseType(404)]
-        [ProducesResponseType(500)]
-        public async Task<IActionResult> GetCustomerAsync(Guid? id)
-        {
-            var response = await _mediator.Send(new GetCustomerQuery(id));
-
-            if (response == null)
-                return NotFound();
-
-            return response.ToOkResult();
-        }
-
+        /// <summary>
+        /// Returns the cards that match with the specified search criteria.
+        /// </summary>
+        /// <param name="request">Search parameters</param>
+        /// <returns>The cards.</returns>
+        /// <response code="200">Returns the cards</response>
+        /// <response code="400">If the request is invalid</response>
+        /// <response code="500">If there was an internal error</response>
         [HttpPost("search-card")]
         [ProducesResponseType(200, Type = typeof(PagedResponse<CardItemModel>))]
         [ProducesResponseType(400)]
@@ -135,6 +166,14 @@ namespace RothschildHouse.API.PaymentGateway.Controllers
             return response.ToOkResult();
         }
 
+        /// <summary>
+        /// Returns an existing card by Id.
+        /// </summary>
+        /// <param name="id">Card Id</param>
+        /// <returns>The card.</returns>
+        /// <response code="200">Returns the card</response>
+        /// <response code="404">If the resource doesn't exist</response>
+        /// <response code="500">If there was an internal error</response>
         [HttpGet("card/{id}")]
         [ProducesResponseType(200, Type = typeof(ISingleResponse<CardDetailsModel>))]
         [ProducesResponseType(404)]
@@ -142,6 +181,47 @@ namespace RothschildHouse.API.PaymentGateway.Controllers
         public async Task<IActionResult> GetCardAsync(Guid? id)
         {
             var response = await _mediator.Send(new GetCardQuery(id));
+
+            if (response == null)
+                return NotFound();
+
+            return response.ToOkResult();
+        }
+
+        /// <summary>
+        /// Returns the customers that match with the specified search criteria.
+        /// </summary>
+        /// <param name="request">Search parameters</param>
+        /// <returns>The customers.</returns>
+        /// <response code="200">Returns the customers</response>
+        /// <response code="400">If the request is invalid</response>
+        /// <response code="500">If there was an internal error</response>
+        [HttpPost("search-customer")]
+        [ProducesResponseType(200, Type = typeof(PagedResponse<CustomerItemModel>))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> SearchCustomersAsync([FromBody] SearchCustomersQuery request)
+        {
+            var response = await _mediator.Send(request);
+
+            return response.ToOkResult();
+        }
+
+        /// <summary>
+        /// Returns an existing customer by Id.
+        /// </summary>
+        /// <param name="id">Customer Id</param>
+        /// <returns>The customer.</returns>
+        /// <response code="200">Returns the customer</response>
+        /// <response code="404">If the resource doesn't exist</response>
+        /// <response code="500">If there was an internal error</response>
+        [HttpGet("customer/{id}")]
+        [ProducesResponseType(200, Type = typeof(ISingleResponse<CustomerDetailsModel>))]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> GetCustomerAsync(Guid? id)
+        {
+            var response = await _mediator.Send(new GetCustomerQuery(id));
 
             if (response == null)
                 return NotFound();
--- Resources/PaymentGateway/RothschildHouse/source/RothschildHouse.API.PaymentGateway/Program.cs
+++ Resources/PaymentGateway/RothschildHouse/source/RothschildHouse.API.PaymentGateway/Program.cs
@@ -8,7 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "RothschildHouse.API.PaymentGateway.xml"));
+});
 
 builder.Services.AddApplicationServices();
 
