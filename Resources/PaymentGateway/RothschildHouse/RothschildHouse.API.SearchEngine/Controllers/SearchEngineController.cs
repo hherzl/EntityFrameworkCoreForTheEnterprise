032c83c3c305b92dcb7b9e362ca959ed9068c124
HH <hans.herzl@live.com>
2023-05-28 15:21:50 -0600

    RothschildHouse: Addition of Search Engine API project

---
+++ Resources/PaymentGateway/RothschildHouse/RothschildHouse.API.SearchEngine/Controllers/SearchEngineController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RothschildHouse.API.SearchEngine.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SearchEngineController : ControllerBase
+    {
+        private readonly ILogger<SearchEngineController> _logger;
+
+        public SearchEngineController(ILogger<SearchEngineController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
