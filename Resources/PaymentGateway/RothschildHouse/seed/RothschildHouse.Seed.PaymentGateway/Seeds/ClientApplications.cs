8d0dff58b217819514d6387c01974f0a9510d2f3
HH <hans.herzl@live.com>
2023-06-07 18:45:24 -0600

    RothschildHouse: Minor fixes

--- Resources/PaymentGateway/RothschildHouse/seed/RothschildHouse.Seed.PaymentGateway/Seeds/ClientApplications.cs
+++ Resources/PaymentGateway/RothschildHouse/seed/RothschildHouse.Seed.PaymentGateway/Seeds/ClientApplications.cs
@@ -11,19 +11,19 @@ namespace RothschildHouse.Seed.PaymentGateway.Seeds
                 yield return new ClientApplication
                 {
                     Id = Guid.Parse("D4159097-96BE-43E0-9E8F-ED4384B0F9C2"),
-                    Name = "Rothschild House GUI"
+                    Name = "Rothschild House PayButton"
                 };
 
                 yield return new ClientApplication
                 {
                     Id = Guid.Parse("9B26240D-6BFA-4F80-AE67-37712D1388A7"),
-                    Name = "UCommerce client"
+                    Name = "eCommerce client"
                 };
 
                 yield return new ClientApplication
                 {
                     Id = Guid.Parse("B74CB3C2-BB35-4436-BCFB-8769B521CA3D"),
-                    Name = "Mocks client"
+                    Name = "Mocks"
                 };
             }
         }
