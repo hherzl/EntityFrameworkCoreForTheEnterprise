e70ee37988063697a4e65bca2d593bcd5e42f4d7
dev.jose-rivera <dev.jose.rivera@gmail.com>
2023-06-22 00:32:52 -0600

    RotchildHouse: adding paging and filters for cards search

--- Resources/PaymentGateway/RothschildHouse/source/RothschildHouse.Library.Common/Clients/Models/PaymentGateway/GetCardsRequest.cs
+++ Resources/PaymentGateway/RothschildHouse/source/RothschildHouse.Library.Common/Clients/Models/PaymentGateway/GetCardsRequest.cs
@@ -8,7 +8,20 @@
             PageNumber = 1;
         }
 
+        public GetCardsRequest(long? cardTypeId, string issuingNetwork, string cardholderName)
+        {
+            PageSize = 25;
+            PageNumber = 1;
+            CardTypeId = cardTypeId;
+            IssuingNetwork = issuingNetwork;
+            CardholderName = cardholderName;          
+        }
+
         public int PageSize { get; set; }
         public int PageNumber { get; set; }
+
+        public long? CardTypeId { get; set; }
+        public string IssuingNetwork { get; set; }
+        public string CardholderName { get; set; }        
     }
 }
