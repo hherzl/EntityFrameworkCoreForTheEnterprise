5922cd275b1a6aff4ad241e6fa9c264f77669ee7
dev.jose-rivera <dev.jose.rivera@gmail.com>
2023-06-25 21:54:18 -0600

    RothschildHouse: Minor fixes in paging and filters for cards search

--- Resources/PaymentGateway/RothschildHouse/source/RothschildHouse.Library.Common/Clients/Models/PaymentGateway/GetCardsViewBagResponse.cs
+++ Resources/PaymentGateway/RothschildHouse/source/RothschildHouse.Library.Common/Clients/Models/PaymentGateway/GetCardsViewBagResponse.cs
@@ -1,10 +1,9 @@
-﻿using RothschildHouse.Domain.Core.Entities;
-using RothschildHouse.Library.Common.Clients.Models.Common;
+﻿using RothschildHouse.Library.Common.Clients.Models.Common;
 
 namespace RothschildHouse.Library.Common.Clients.Models.PaymentGateway
 {
     public record GetCardsViewBagResponse : Response
     {
-        public List<VCardType> CardTypes { get; set; }        
+        public List<ListItem<long?>> CardTypes { get; set; }        
     }
 }
