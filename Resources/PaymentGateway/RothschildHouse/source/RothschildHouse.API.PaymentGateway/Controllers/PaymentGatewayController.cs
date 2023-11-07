4ad283182d755f2950b5e234723673f5c73c4fec
HH <hans.herzl@live.com>
2023-11-07 16:56:22 -0600

    Payment gateway: addition of API

---
+++ Resources/PaymentGateway/RothschildHouse/source/RothschildHouse.API.PaymentGateway/Controllers/PaymentGatewayController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RothschildHouse.API.PaymentGateway.Controllers;
+
+[ApiController]
+[Route("api/v1")]
+public class PaymentGatewayController : ControllerBase
+{
+    public PaymentGatewayController()
+    {
+    }
+
+    [HttpGet("foo")]
+    public string Foo()
+        => "Foo bar baz";
+}
