70213e6a3c575d2e16d008af5c8191400bac1bc4
HH <hans.herzl@live.com>
2023-11-07 17:53:34 -0600

    Payment gateway: addition of exceptions

---
+++ Resources/PaymentGateway/RothschildHouse/source/RothschildHouse.Domain/Exceptions/RothschildHouseException.cs
@@ -0,0 +1,18 @@
+﻿namespace RothschildHouse.Domain.Exceptions;
+
+public class RothschildHouseException : Exception
+{
+    public RothschildHouseException()
+    {
+    }
+
+    public RothschildHouseException(string message)
+        : base(message)
+    {
+    }
+
+    public RothschildHouseException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
