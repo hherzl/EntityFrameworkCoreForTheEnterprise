151285eb5558bd4dc2f3b16b941e97be351ab336
HH <hans.herzl@live.com>
2023-12-17 22:37:52 -0600

    Resources: Addition of Home component for Payment Gateway GUI

--- Resources/PaymentGateway/rothschild-house-gui/src/app/app-routing.module.ts
+++ Resources/PaymentGateway/rothschild-house-gui/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Resources/PaymentGateway/rothschild-house-gui/src/app/app.module.ts
+++ Resources/PaymentGateway/rothschild-house-gui/src/app/app.module.ts
@@ -10,10 +10,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
