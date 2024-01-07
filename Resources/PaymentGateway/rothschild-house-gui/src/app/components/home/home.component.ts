aff5add721c05d79aa5250b60be1cd9339cd8be4
HH <hans.herzl@live.com>
2024-01-07 07:01:30 -0600

    Resources: addition of signalR listener in home component

--- Resources/PaymentGateway/rothschild-house-gui/src/app/components/home/home.component.ts
+++ Resources/PaymentGateway/rothschild-house-gui/src/app/components/home/home.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as signalR from '@microsoft/signalr';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  private hubConnection!: signalR.HubConnection;
 
+  public startConnection = () => {
+    this.hubConnection = new signalR.HubConnectionBuilder()
+      .withUrl('https://localhost:7251/txnhub', {
+        skipNegotiation: true,
+        transport: signalR.HttpTransportType.WebSockets
+      })
+      .build();
+    
+    this.hubConnection
+      .start()
+      .then(() => {
+        console.log('Connection started!');
+      })
+      .catch(err => {
+        console.log(`Error while starting connection: ${err}`);
+      })
+  }
+
+  public addListener = () => {
+    this.hubConnection.on('receiveTxn', (clientApplication, amount, currency) => {
+      console.log(`'${clientApplication}': ${amount} ${currency}`);
+    });
+  }
+
+  public ngOnInit(): void {
+    this.startConnection();
+    this.addListener();
+  }
 }
